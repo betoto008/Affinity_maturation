ce7f2a0380ece3a0ca16cf99856febb7cdb2b611
Roberto Moran Tovar <r.morantovar@uni-koeln.de>
2021-06-07 22:02:38 +0200

    working on geometric exponent mac20

--- Codes/Python/analysis_complexity.py
+++ Codes/Python/analysis_complexity.py
@@ -1,4 +1,8 @@
 #---------Import---------
+import sys
+sys.path.append('../Codes/')
+sys.path.append('../Codes/Python/')
+from Immuno_models import*
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,20 +10,7 @@ import matplotlib.ticker as mtick
 from matplotlib.ticker import PercentFormatter
 from matplotlib.lines import Line2D
 
-#---------Functions---------
-def my_linear_func(x, a, b):
-    return a + b*x
-def my_quadratic_func(x, a, b, c):
-    return np.log(a)+np.log(np.sqrt(-b)) + b*(x-c)**2
-def my_plot_layout(ax, yscale = 'linear', xscale = 'linear', ticks_labelsize = 24,
-                   xlabel = '', ylabel = '', title = '', x_fontsize=24, y_fontsize = 24,
-                   t_fontsize = 24):
-    ax.tick_params(labelsize = ticks_labelsize)
-    ax.set_yscale(yscale)
-    ax.set_xscale(xscale)
-    ax.set_xlabel(xlabel, fontsize = x_fontsize)
-    ax.set_ylabel(ylabel, fontsize = y_fontsize)
-    ax.set_title(title, fontsize = y_fontsize)
+
 #------------------------------------
 
 Text_files_path = '../../../../../Dropbox/Research/Evolution_Immune_System/Text_files/Complexity/'
